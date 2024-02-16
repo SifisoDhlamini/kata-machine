2c424ec0bfbd8c2ffbe1a00b949f1eca9aaab179
Sifiso Lucolo Dhlamini <sifisoldhlamini@outlook.com>
2024-02-16 21:59:26 +0800

    bubble Sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,16 @@
 export default function bubble_sort(arr: number[]): void {
-
+    let swapped: boolean;
+    do {
+        swapped = false;
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                const temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
 }
+
+//if there was no swap in the last iteration, the array is sorted
